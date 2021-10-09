fe5768ddd0b19df945334f1a6e218dfd5452a8ab
AlekDevs <amacwilliams06@gmail.com>
2021-10-08 21:45:55 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import time 
+
+
+def countdown(t): 
+    
+    while t: 
+        mins, secs = divmod(t, 60) 
+        timer = '{:02d}:{:02d}'.format(mins, secs) 
+        print(timer, end="\r") 
+        time.sleep(1) 
+        t -= 1
+    
+    print('Timers up') 
+
+
+
+t = 10 #ammount of time 
+
+
+countdown(int(t))
